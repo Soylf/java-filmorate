95fbfcca0bb94689a7fea638d338c842b42a0bf8
Soyl <Soylgr@yandex.ru>
2023-11-05 22:21:59 +0300

    fix

--- src/main/java/ru/yandex/practicum/filmorate/model/User.java
+++ src/main/java/ru/yandex/practicum/filmorate/model/User.java
@@ -1,9 +1,7 @@
 package ru.yandex.practicum.filmorate.model;
 
-import jakarta.validation.constraints.NotBlank;
 import lombok.AllArgsConstructor;
 import lombok.Data;
-import lombok.experimental.NonFinal;
 
 import java.time.LocalDate;
 
