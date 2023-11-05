3f85f81e27ecf9789e28a98a3af154a94f1629d2
Soyl <Soylgr@yandex.ru>
2023-11-05 23:51:17 +0300

    fix

--- src/main/java/ru/yandex/practicum/filmorate/model/AbstractModel.java
+++ src/main/java/ru/yandex/practicum/filmorate/model/AbstractModel.java
@@ -1,11 +1,17 @@
 package ru.yandex.practicum.filmorate.model;
 
-import lombok.AllArgsConstructor;
-import lombok.Data;
 
-@Data
-@AllArgsConstructor
+import lombok.Getter;
+import lombok.Setter;
+import lombok.experimental.NonFinal;
+
+@Getter
+@Setter
+@NonFinal
 public class AbstractModel {
     private Integer id;
     private String name;
+
+    public AbstractModel(Integer id, String name) {
+    }
 }
