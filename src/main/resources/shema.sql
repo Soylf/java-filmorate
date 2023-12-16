c78c12908ffb4d7a2c2c53a5ecc88ed9370fcf50
Soyl <Soylgr@yandex.ru>
2023-12-16 17:01:30 +0300

    fix

--- src/main/resources/shema.sql
+++ src/main/resources/shema.sql
@@ -59,3 +59,18 @@ CREATE TABLE IF NOT EXISTS Friendship
     user_id       INTEGER,
     FOREIGN KEY (user_id) REFERENCES User_Filmorate(id) ON DELETE CASCADE
 );
+
+INSERT INTO Genre (genre_name)
+VALUES ('Комедия'),
+    ('Драма'),
+    ('Мультфильм'),
+    ('Триллер'),
+    ('Документальный'),
+    ('Боевик');
+
+INSERT INTO Mpa (name)
+VALUES ('G'),
+       ('PG'),
+       ('PG-13'),
+       ('R'),
+       ('NC-17');
