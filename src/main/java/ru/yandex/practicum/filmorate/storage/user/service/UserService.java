ca422c84d5c15153aec6ba578a38ad11c39658c1
Soyl <Soylgr@yandex.ru>
2023-12-19 23:53:28 +0300

    fix

--- src/main/java/ru/yandex/practicum/filmorate/storage/user/service/UserService.java
+++ src/main/java/ru/yandex/practicum/filmorate/storage/user/service/UserService.java
@@ -4,15 +4,12 @@ package ru.yandex.practicum.filmorate.storage.user.service;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.dao.EmptyResultDataAccessException;
 import org.springframework.stereotype.Service;
-import ru.yandex.practicum.filmorate.errorException.ValidationException;
 import ru.yandex.practicum.filmorate.errorException.exception.EntityNotFoundException;
 import ru.yandex.practicum.filmorate.model.User;
 import ru.yandex.practicum.filmorate.storage.user.UserStorage;
 
-import java.time.LocalDate;
 import java.util.ArrayList;
 import java.util.List;
-import java.util.Set;
 
 
 @Service
